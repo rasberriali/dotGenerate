f45d7dfa2e94aef8101f3ea9702a0c61a6b67dcc
rasberriali <julesalysa@gmail.com>
2024-12-28 21:21:36 +0800

    aa

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Project = require("../models/Project");
 const Joi = require("joi");
+const mongoose = require("mongoose");
+
 
 // Validation schema using Joi
 const projectSchema = Joi.object({
